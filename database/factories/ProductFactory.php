19b94d48f12d9fa8be8aaa7ef456aed29bae7e50
Christian <cristian@devsquad.com>
2018-10-25 15:43:26 -0300

    Database seeded with Faker library

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,8 +2,13 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Models\Product::class, function (Faker $faker) {
     return [
-
+        'name' => $faker->word,
+        'subName' => $faker->word,
+        'price' => $faker->numberBetween(100, 300),
+        'description' => $faker->paragraph,
+        'image' => $faker->imageUrl($width=640, $height=480),
+        'tag' => $faker->word
     ];
 });
--- database/migrations/2018_10_24_214716_create_products_table.php
+++ database/migrations/2018_10_24_214716_create_products_table.php
@@ -10,11 +10,14 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+
             $table->string('name');
             $table->string('subName');
+            $table->integer('price');
             $table->text('description');
             $table->string('image');
             $table->string('tag');
+
             $table->timestamps();
         });
     }
